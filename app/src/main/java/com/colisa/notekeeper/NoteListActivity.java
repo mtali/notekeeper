42ed3be8faf556a2a20b46accd853bded022e400
mtali <emmanuelmtali@yahoo.com>
2019-08-12 00:05:18 +0300

    Initial commit with basic structure

---
+++ app/src/main/java/com/colisa/notekeeper/NoteListActivity.java
@@ -0,0 +1,63 @@
+package com.colisa.notekeeper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class NoteListActivity extends AppCompatActivity {
+    public static final String NOTE_POSITION = "com.colisa.notekeeper.NOTE_POSITION";
+    private ArrayAdapter<NoteInfo> mAdapterNotes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NoteListActivity.this, NoteActivity.class));
+            }
+        });
+
+        initializeDisplayContent();
+    }
+
+    private void initializeDisplayContent() {
+        final ListView listNotes = findViewById(R.id.list_notes);
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+
+        mAdapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
+
+        listNotes.setAdapter(mAdapterNotes);
+
+        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
+                intent.putExtra(NoteListActivity.NOTE_POSITION, position);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAdapterNotes.notifyDataSetChanged();
+    }
+}
