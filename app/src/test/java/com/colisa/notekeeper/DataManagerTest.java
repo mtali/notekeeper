cff7e3cf5c2b966737024c8f53705637cb0fb4b2
mtali <emmanuelmtali@yahoo.com>
2019-08-18 22:53:28 +0300

    Refactor DataManager to static field and use BeforeClass annotation to get it

--- app/src/test/java/com/colisa/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/colisa/notekeeper/DataManagerTest.java
@@ -1,36 +1,41 @@
 package com.colisa.notekeeper;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class DataManagerTest {
+    private static DataManager sDataManager;
+
+    @BeforeClass
+    public static void classSetup() {
+        sDataManager = DataManager.getInstance();
+    }
 
     @Before
     public void setUp() {
         // Make sure that each test run with fresh data
-        DataManager dm = DataManager.getInstance();
-        dm.getNotes().clear();
-        dm.initializeExampleNotes();
+        sDataManager.getNotes().clear();
+        sDataManager.initializeExampleNotes();
     }
 
     @Test
     public void createNewNote() {
-        final DataManager dm = DataManager.getInstance();
-        final CourseInfo course = dm.getCourse("android_async");
+        final CourseInfo course = sDataManager.getCourse("android_async");
         final String noteTitle = "Test note title";
         final String noteText = "This is the body text for test note";
 
-        int noteIndex = dm.createNewNote();
-        NoteInfo newNote = dm.getNotes().get(noteIndex);
+        int noteIndex = sDataManager.createNewNote();
+        NoteInfo newNote = sDataManager.getNotes().get(noteIndex);
         newNote.setCourse(course);
         newNote.setTitle(noteTitle);
         newNote.setText(noteText);
 
         // We just created a note
         // We need to confirm our new created note is on data manager
-        NoteInfo compareNote = dm.getNotes().get(noteIndex);
+        NoteInfo compareNote = sDataManager.getNotes().get(noteIndex);
         assertEquals(compareNote.getCourse(), newNote.getCourse());
         assertEquals(compareNote.getTitle(), newNote.getTitle());
         assertEquals(compareNote.getText(), newNote.getText());
@@ -38,30 +43,29 @@ public class DataManagerTest {
 
     @Test
     public void findSimilarNotes() {
-        final DataManager dm = DataManager.getInstance();
-        final CourseInfo course = dm.getCourse("android_async");
+        final CourseInfo course = sDataManager.getCourse("android_async");
         final String noteTitle = "Test note title";
         final String noteText1 = "This is the body text for test note";
         final String noteText2 = "This is the body of my second test note";
 
-        int noteIndex1 = dm.createNewNote();
-        NoteInfo note1 = dm.getNotes().get(noteIndex1);
+        int noteIndex1 = sDataManager.createNewNote();
+        NoteInfo note1 = sDataManager.getNotes().get(noteIndex1);
         note1.setCourse(course);
         note1.setText(noteText1);
         note1.setTitle(noteTitle);
 
-        int noteIndex2 = dm.createNewNote();
-        NoteInfo note2 = dm.getNotes().get(noteIndex2);
+        int noteIndex2 = sDataManager.createNewNote();
+        NoteInfo note2 = sDataManager.getNotes().get(noteIndex2);
         note2.setCourse(course);
         note2.setTitle(noteTitle);
         note2.setText(noteText2);
 
         // We just created two notes with same course and title
         // Let's test DataManager ability to find correct note
-        int foundIndex1 = dm.findNote(note1);
+        int foundIndex1 = sDataManager.findNote(note1);
         assertEquals(noteIndex1, foundIndex1);
 
-        int foundIndex2 = dm.findNote(note2);
+        int foundIndex2 = sDataManager.findNote(note2);
         assertEquals(noteIndex2, foundIndex2);
     }
 }
