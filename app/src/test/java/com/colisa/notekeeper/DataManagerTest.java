1c1dc170318e8dbffe5da6575b807dbad8b4e00f
mtali <emmanuelmtali@yahoo.com>
2019-08-18 21:58:38 +0300

    Test note create for data manager

---
+++ app/src/test/java/com/colisa/notekeeper/DataManagerTest.java
@@ -0,0 +1,31 @@
+package com.colisa.notekeeper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataManagerTest {
+
+    @Test
+    public void createNewNote() {
+        final DataManager dm = DataManager.getInstance();
+        final CourseInfo course = dm.getCourse("android_async");
+        final String noteTitle = "Test note title";
+        final String noteText = "This is the body text for test note";
+
+        int noteIndex = dm.createNewNote();
+        NoteInfo newNote = dm.getNotes().get(noteIndex);
+        newNote.setCourse(course);
+        newNote.setTitle(noteTitle);
+        newNote.setText(noteText);
+
+        // We just created a note
+        // We need to confirm our new created note is on data manager
+        NoteInfo compareNote = dm.getNotes().get(noteIndex);
+        assertEquals(newNote.getCourse(), compareNote.getCourse());
+        assertEquals(newNote.getTitle(), compareNote.getTitle());
+        assertEquals(newNote.getText(), compareNote.getText());
+
+
+    }
+}
