c406d992b02909857a5c76a8f298af238ff4657b
mtali <emmanuelmtali@yahoo.com>
2019-08-18 22:48:10 +0300

    Add test to validate datamanager findNote if it return expected note

--- app/src/test/java/com/colisa/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/colisa/notekeeper/DataManagerTest.java
@@ -1,10 +1,19 @@
 package com.colisa.notekeeper;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class DataManagerTest {
+
+    @Before
+    public void setUp() {
+        // Make sure that each test run with fresh data
+        DataManager dm = DataManager.getInstance();
+        dm.getNotes().clear();
+        dm.initializeExampleNotes();
+    }
 
     @Test
     public void createNewNote() {
@@ -25,6 +34,34 @@ public class DataManagerTest {
         assertEquals(compareNote.getCourse(), newNote.getCourse());
         assertEquals(compareNote.getTitle(), newNote.getTitle());
         assertEquals(compareNote.getText(), newNote.getText());
+    }
 
+    @Test
+    public void findSimilarNotes() {
+        final DataManager dm = DataManager.getInstance();
+        final CourseInfo course = dm.getCourse("android_async");
+        final String noteTitle = "Test note title";
+        final String noteText1 = "This is the body text for test note";
+        final String noteText2 = "This is the body of my second test note";
+
+        int noteIndex1 = dm.createNewNote();
+        NoteInfo note1 = dm.getNotes().get(noteIndex1);
+        note1.setCourse(course);
+        note1.setText(noteText1);
+        note1.setTitle(noteTitle);
+
+        int noteIndex2 = dm.createNewNote();
+        NoteInfo note2 = dm.getNotes().get(noteIndex2);
+        note2.setCourse(course);
+        note2.setTitle(noteTitle);
+        note2.setText(noteText2);
+
+        // We just created two notes with same course and title
+        // Let's test DataManager ability to find correct note
+        int foundIndex1 = dm.findNote(note1);
+        assertEquals(noteIndex1, foundIndex1);
+
+        int foundIndex2 = dm.findNote(note2);
+        assertEquals(noteIndex2, foundIndex2);
     }
 }
