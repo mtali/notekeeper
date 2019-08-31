b5eceeb72691ad90cbc9ebba8c820119a45e10c1
mtali <emmanuelmtali@yahoo.com>
2019-08-31 20:20:42 +0300

    Implement insert new note functionality

--- app/src/main/java/com/colisa/notekeeper/NoteActivity.java
+++ app/src/main/java/com/colisa/notekeeper/NoteActivity.java
@@ -147,8 +147,13 @@ public class NoteActivity extends AppCompatActivity implements LoaderManager.Loa
     }
 
     private void createNewNote() {
-        DataManager dm = DataManager.getInstance();
-        mNoteId = dm.createNewNote();
+        ContentValues values = new ContentValues();
+        values.put(NoteInfoEntry.COLUMN_COURSE_ID, "");
+        values.put(NoteInfoEntry.COLUMN_NOTE_TITLE, "");
+        values.put(NoteInfoEntry.COLUMN_NOTE_TEXT, "");
+
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        mNoteId = (int) db.insert(NoteInfoEntry.TABLE_NAME, null, values);
     }
 
 
