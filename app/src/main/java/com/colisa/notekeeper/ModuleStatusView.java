4ed764f6d4e3448f58cccbabba27fa6f87060c44
mtali <emmanuelmtali@yahoo.com>
2019-09-09 23:21:12 +0300

    Add custom view for module status and implement size adaptability

---
+++ app/src/main/java/com/colisa/notekeeper/ModuleStatusView.java
@@ -0,0 +1,158 @@
+package com.colisa.notekeeper;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+public class ModuleStatusView extends View {
+
+    private static final String TAG = ModuleStatusView.class.getSimpleName();
+    public static final int EDIT_MODE_MODULE_CONTENT = 7;
+    private boolean[] mModuleStatus;
+    private float mOutlineWidth;
+    private float mShapeSize;
+    private float mSpacing;
+    private Rect[] mModuleRectangle;
+    private int mOutlineColor;
+    private Paint mPaintOutline;
+    private int mFillColor;
+    private Paint mPaintFill;
+    private float mRadius;
+    private int mMaxHorizontalModules;
+
+    public ModuleStatusView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public ModuleStatusView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public ModuleStatusView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    /**
+     * View initialization
+     * @param attrs attributes
+     * @param defStyle styles
+     */
+    private void init(AttributeSet attrs, int defStyle) {
+
+        if (isInEditMode() || true) {
+            setupEditModeValues();
+        }
+
+        final TypedArray a = getContext().obtainStyledAttributes(
+                attrs, R.styleable.ModuleStatusView, defStyle, 0
+        );
+        a.recycle();
+
+        mOutlineWidth = 6f;
+        mShapeSize = 144f;
+        mSpacing = 30f;
+        mRadius = (mShapeSize + mOutlineWidth) / 2;
+
+        mOutlineColor = Color.BLACK;
+
+        mPaintOutline = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintOutline.setStyle(Paint.Style.STROKE);
+        mPaintOutline.setStrokeWidth(mOutlineWidth);
+        mPaintOutline.setColor(mOutlineColor);
+
+        mFillColor = getContext().getResources().getColor(R.color.application_orange);
+        mPaintFill = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintFill.setStyle(Paint.Style.FILL);
+        mPaintFill.setColor(mFillColor);
+
+    }
+
+    private void setupEditModeValues() {
+        boolean[] exampleModuleValues = new boolean[EDIT_MODE_MODULE_CONTENT];
+        int middle = EDIT_MODE_MODULE_CONTENT / 2;
+        for (int i = 0; i < middle; i++) {
+            exampleModuleValues[i] = true;
+        }
+        setModuleStatus(exampleModuleValues);
+
+    }
+
+    private void setupModuleRectangles(int w, int h) {
+
+        int availableWidth = w - getPaddingLeft() - getPaddingRight();
+        int horizontalModulesThatCanFit = (int) (availableWidth / (mShapeSize + mSpacing));
+        int maxHorizontalModules = Math.min(horizontalModulesThatCanFit, mModuleStatus.length);
+
+        mModuleRectangle = new Rect[mModuleStatus.length];
+        for(int moduleIndex = 0; moduleIndex < mModuleRectangle.length; moduleIndex++) {
+            int column = moduleIndex % maxHorizontalModules;
+            int row = moduleIndex / maxHorizontalModules;
+            int x = getPaddingLeft() + (int) (column * (mSpacing + mShapeSize));
+            int y = getPaddingTop() + (int) (row * (mShapeSize + mSpacing));
+            mModuleRectangle[moduleIndex] = new Rect(x, y, x + (int) mShapeSize, y + (int) mShapeSize);
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Log.d(TAG, "Inside onDraw()");
+        super.onDraw(canvas);
+
+        for(int moduleIndex = 0; moduleIndex < mModuleRectangle.length; moduleIndex++) {
+            float x = mModuleRectangle[moduleIndex].centerX();
+            float y = mModuleRectangle[moduleIndex].centerY();
+            if(mModuleStatus[moduleIndex]) {
+                canvas.drawCircle(x, y, mRadius, mPaintFill);
+            }
+            canvas.drawCircle(x, y, mRadius, mPaintOutline);
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Log.d(TAG, "Inside onMeasure()");
+        int desiredWidth;
+        int desiredHeight;
+
+        int specWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int availableWidth = specWidth - getPaddingLeft() - getPaddingRight();
+
+        int horizontalModulesThatCanFit = (int)(availableWidth / (mShapeSize + mSpacing));
+        mMaxHorizontalModules = Math.min(horizontalModulesThatCanFit, mModuleStatus.length);
+        desiredWidth = (int)((mMaxHorizontalModules * (mShapeSize + mSpacing)) - mSpacing) ;
+        desiredWidth += getPaddingLeft() + getPaddingRight();
+
+        int rows = ((mModuleStatus.length - 1) / mMaxHorizontalModules) + 1;
+        desiredHeight = (int)((rows * (mShapeSize + mSpacing)) - mSpacing);
+        desiredHeight += getPaddingTop() + getPaddingBottom();
+
+        int width = resolveSizeAndState(desiredWidth, widthMeasureSpec, 0);
+        int height = resolveSizeAndState(desiredHeight, heightMeasureSpec, 0);
+
+        Log.d(TAG, "Leaving onMeasure() with -> width: " + width + " height: " + height);
+        setMeasuredDimension(width, height);
+
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldWidth, int oldHeight) {
+        Log.d(TAG, "Executing onSizeChanged() with -> width: " + w + " height: " + h);
+        setupModuleRectangles(w, h);
+    }
+
+    public void setModuleStatus(boolean[] moduleStatus) {
+        mModuleStatus = moduleStatus;
+    }
+}
